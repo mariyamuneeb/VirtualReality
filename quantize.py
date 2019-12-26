3f177ff4c4f6b6e9c5a1294d27525dbf9c77d25e
TayyabMujahid <tayyab.mujahid@gmail.com>
2019-12-26 17:37:19 +0300

    quantization

---
+++ quantize.py
@@ -0,0 +1,34 @@
+
+import tensorflow as tf
+
+
+# tf.RegisterGradient('QuantizeGradient')
+
+def clamp(input_tensor, num_bits = 8):
+    input_tensor = tf.clip_by_value(input_tensor, clip_value_min=0, clip_value_max =2 ** (num_bits) - 1)
+    input_tensor = ((input_tensor * 2 ** (num_bits)).round()) / 2 ** (num_bits)
+    return input_tensor
+
+def to_fixed_point(input_tensor,ibits,fbits):
+    input_tensor_i = tf.sign(input_tensor)*clamp(tf.floor(tf.abs(input_tensor)),ibits)
+    input_tensor_f = tf.sign(input_tensor)*clamp(tf.abs(input_tensor)-tf.floor(tf.abs(input_tensor)),fbits)
+    return input_tensor_i + input_tensor_f
+
+@tf.RegisterGradient('fixed_point_grad')
+def _qunatize_grad(ops,grad):
+    return grad
+
+def quantize(input_tensor):
+    G = tf.compat.v1.get_default_graph()
+    with G.over_ride_gradient_map
+
+
+
+
+
+
+def quantize_grad():
+    None
+
+def quantize(x,n):
+    max_ =
